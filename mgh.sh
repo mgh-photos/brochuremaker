39f1204bd90f54206750fd230b677bf395cc9344
mgh-photos <42238310+mgh-photos@users.noreply.github.com>
2019-10-09 09:19:36 +0100

    --skip-ci

--- mgh.sh
+++ mgh.sh
@@ -44,5 +44,6 @@ gsutil -o Credentials:gs_service_key_file=$HOME/clone/gsutil-key.json acl ch -u 
 #
 sleep 25s
 wget "https://xousian.appspot.com/listbucket/$mghref?a=renew"
+cd $HOME/clone
 git filter-branch -f --tree-filter 'rm -f brochure.zip' HEAD
 # --skip-ci
